c5af18d00fa1646cb36a494abe092e823ec81dec
mango2309 <sebastian.largo@udla.edu.ec>
2025-04-09 01:06:42 -0500

    Metodo Create agregado al servicio

---
+++ src/app/features/category/services/category.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { AddCategoryRequest } from '../models/add-category-request.model';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Category } from '../models/category.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor(private http: HttpClient) { }
+
+  addCategory(model: AddCategoryRequest) : Observable<void>{
+    return this.http.post<void>('https://localhost:7034/api/categorias', model);
+
+  }
+
+  getallCategories(): Observable<Category[]>{
+    return this.http.get<Category[]>('https://localhost:7034/api/Categorias');
+  }
+}
